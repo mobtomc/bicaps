112eaefa8ebc2547e607bc0713a1507b612d3dc4
aryan bhat <aryabhat99@gmail.com>
2024-07-02 14:53:45 +0530

    minor changes

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const app=express();
+const cors=require('cors');
+const port=5000;
+//middleware
+app.use(cors());
+app.use(express.json());
+app.listen(port,()=>{
+ console.log(`server is running on port:${port}`)
+})
