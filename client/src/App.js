97e0b6145eb126b5f2f32f21eab2f1672b937091
aryan bhat <aryabhat99@gmail.com>
2024-07-19 12:31:50 +0530

    authentication,responsive navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,56 @@
-
-import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Nav from "./Components/Nav";
 import Add from "./Pages/Add";
-import Records from "./Pages/Records"
-import Home from "./Pages/Home"
-import New from "./Pages/New"
-import Entity from "./Pages/Entity"
-import Update from './Pages/Update'
-import Replace from './Pages/Replace'
-import ProjectType from './Pages/ProjectType'
+import Records from "./Pages/Records";
+import Home from "./Pages/Home";
+import New from "./Pages/New";
+import Entity from "./Pages/Entity";
+import Update from './Pages/Update';
+import Replace from './Pages/Replace';
+import ProjectType from './Pages/ProjectType';
 import AddProject from './Pages/AddProject';
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+} from "@clerk/clerk-react";
+
+// Check for the publishable key
+if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
+const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-      <Nav/>
-      <Routes>
-      <Route exact path="/" element={<Home />}></Route>
-          <Route path="/Add" element={<Add/>}></Route>
-          <Route path="/Records" element={<Records />}></Route>
-          <Route path="/Entity" element={<Entity/>}></Route>
-          <Route path="/New" element={<New />}></Route>
-          <Route path="/update/:id" element={<Update/>} />
-          <Route path="/replace" element={<Replace/>}></Route>
-          <Route path="/projecttype" element={<ProjectType/>}></Route>
-          <Route path="/addproject" element={<AddProject/>}></Route>
-      </Routes>
-      </BrowserRouter>
-    </div>
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <SignedIn>
+        <div className="App">
+          <BrowserRouter>
+            <Nav />
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/Add" element={<Add />} />
+              <Route path="/Records" element={<Records />} />
+              <Route path="/Entity" element={<Entity />} />
+              <Route path="/New" element={<New />} />
+              <Route path="/update/:id" element={<Update />} />
+              <Route path="/replace" element={<Replace />} />
+              <Route path="/projecttype" element={<ProjectType />} />
+              <Route path="/addproject" element={<AddProject />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </ClerkProvider>
   );
 }
 
 export default App;
+
